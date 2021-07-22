85a1354e8b24bb6b36f623be385bab3518bd6688
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2021-07-22 21:54:15 +0700

    setup project

---
+++ PanicAway/Core/Extension/Extension+UIView.swift
@@ -0,0 +1,84 @@
+//
+//  Extension+UIView.swift
+//  PanicAway
+//
+//  Created by Randy Efan Jayaputra on 22/07/21.
+//
+
+import UIKit
+
+public extension UIView {
+    private typealias Action = (() -> Void)?
+    private struct AssociatedObjectKeys {
+        static var tapGestureRecognizer = "UIView_tapGesture"
+    }
+    
+    private var tapGestureRecognizerAction: Action? {
+        set {
+            if let newValue = newValue {
+                objc_setAssociatedObject(
+                    self,
+                    &AssociatedObjectKeys.tapGestureRecognizer,
+                    newValue,
+                    objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN
+                )
+            }
+        }
+        get {
+            let tapGestureRecognizerActionInstance = objc_getAssociatedObject(
+                self,
+                &AssociatedObjectKeys.tapGestureRecognizer
+            ) as? Action
+            return tapGestureRecognizerActionInstance
+        }
+    }
+    
+    func onTap(action: (() -> Void)?) {
+        self.isUserInteractionEnabled = true
+        self.tapGestureRecognizerAction = action
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        self.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc private func handleTapGesture(sender: UITapGestureRecognizer) {
+        if let action = self.tapGestureRecognizerAction {
+            action?()
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            guard let color = newValue else {
+                layer.borderColor = nil
+                return
+            }
+            guard String(describing: type(of: color)) != "__NSCFType" else { return }
+            layer.borderColor = color.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.masksToBounds = true
+            layer.cornerRadius = abs(CGFloat(Int(newValue * 100)) / 100)
+        }
+    }
+}
+
+
