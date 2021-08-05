6b6774404615811d3285a6d19b7c985a63aeb1a4
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2021-08-05 09:34:54 +0700

    handle sync iOS watch settings

--- PanicAway WatchKit Extension/Controller/InterfaceController.swift
+++ PanicAway WatchKit Extension/Controller/InterfaceController.swift
@@ -7,20 +7,48 @@
 
 import WatchKit
 import Foundation
-
+import WatchConnectivity
 
 class InterfaceController: WKInterfaceController {
-
+    
+    var watchSession: WCSession? {
+        didSet {
+            if let session = watchSession {
+                session.delegate = self
+                session.activate()
+            }
+        }
+    }
+    
     override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        if let session = watchSession {
+            session.delegate = self
+            session.activate()
+        }
+        
         // Configure interface objects here.
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        watchSession = WCSession.default
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
     }
+}
 
+extension InterfaceController: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) { }
+    
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+        DispatchQueue.main.async {
+            print("watch received app context: ", applicationContext)
+        }
+    }
 }
