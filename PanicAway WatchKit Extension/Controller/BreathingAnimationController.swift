94db273ae9f48041fe7e4b83c09972ef7d2f17e4
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2021-08-12 13:15:17 +0700

    WIP: Handle state animation in watch - randy

--- PanicAway WatchKit Extension/Controller/BreathingAnimationController.swift
+++ PanicAway WatchKit Extension/Controller/BreathingAnimationController.swift
@@ -9,6 +9,12 @@ import UIKit
 import WatchKit
 import HealthKit
 
+enum BreathingStateWatch: String {
+    case breatheIn = "Breathe In"
+    case breatheOut = "Breathe Out"
+    case hold = "Hold"
+}
+
 class BreathingAnimationController: WKInterfaceController {
 
     
@@ -18,13 +24,37 @@ class BreathingAnimationController: WKInterfaceController {
     @IBOutlet weak var heartRateLabel: WKInterfaceLabel!
     @IBOutlet weak var tapBreathingAnimationGesture: WKTapGestureRecognizer!
     
-    var isWorkoutLive = false
+    var isWorkoutLive = false {
+        didSet {
+            if !isWorkoutLive {
+                informationLabel.setText("Tap To Start")
+            }
+        }
+    }
+    
+    // isFinish di set true ketika sudah satu putaran breathing (in-hold-out),
+    var isFinish: Bool = false
+    
     var heartRate: Double = 120
     var isHeartBeatAnimated = false
     var duration: Double{
         return (60/heartRate)/2
     }
     var timer:Timer?
+    var breathingCycle: Int?
+    var data = BreathingLoader()
+    
+    // Ini state harus diset ke nil, kalau dia ga breathing ya, soalnya di todo kita bisa balikin dia ke state awal
+    var state: BreathingStateWatch? {
+        didSet {
+            guard let _ = state else {
+                //TODO: - Handle With State when they doesnt in breathing mode
+                return
+            }
+            self.updateView()
+        }
+    }
+    var breathingTechnique: BreathingModel?
     
     let healthKitManager = HealthKitManager()
     
@@ -33,32 +63,73 @@ class BreathingAnimationController: WKInterfaceController {
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         requestHealthKit()
+        data.loadDataBreath()
+        breathingTechnique = data.entries[0]
+        breathingCycle = 2
     }
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
     }
     
     @IBAction func tapToStartClicked(_ sender: Any) {
-           if isWorkoutLive {
-               informationLabel.setText("Start")
-               isWorkoutLive = false
-               healthKitManager.endWorkoutSession()
-               isHeartBeatAnimated = false
-               stopHeartBeatAnimation()
-               timer?.invalidate()
-           }else{
-               informationLabel.setText("Stop")
-               isWorkoutLive = true
-               healthKitManager.startWorkoutSession()
-               isHeartBeatAnimated = true
-               startHeartBeatAnimation()
-               startTimer()
-           }
-           
-       }
+        // State ketika tap start
+        // Cek dulu apakah isWorkoutLive nya true atau tidak (kalau true berarti dia sedang breath -> dan harusnya dia finish breath, kalau false -> harusnya dia mulai breath)
+        if isWorkoutLive {
+            // Masuk kesini ketika sedang berjalan breathing tapi user tap watch
+            // Update Label
+            informationLabel.setText("Start")
+            print("Finish")
+            // Update Variable
+            isWorkoutLive = false
+            isHeartBeatAnimated = false
+            
+            // Workout session
+            healthKitManager.endWorkoutSession()
+            stopHeartBeatAnimation()
+            
+            // Validate Timer
+            timer?.invalidate()
+        } else {
+            // Masuk kesini ketika sedang tidak workout tapi user tap watch
+            // Update Label
+            informationLabel.setText("Stop")
+            
+            // Update Variable
+            isWorkoutLive = true
+            isHeartBeatAnimated = true
+            
+            // Handle Breathing State & update view
+            state = .breatheIn
+            updateView()
+            
+            // Workout stuff
+            healthKitManager.startWorkoutSession()
+            startHeartBeatAnimation()
+            
+            // Timer stuff
+            startTimer()
+        }
+    }
+    
+    func updateView() {
+        if isWorkoutLive {
+            guard let breathingState = state else { return }
+            switch breathingState {
+            case .breatheIn:
+                //TODO: - Handle Setup Animation Breathe In Here
+                print("Breathe In")
+                
+            case .breatheOut:
+                //TODO: - Handle Setup Animation Breathe Out Here
+                print("Breathe Out")
+                
+            case .hold:
+                //TODO: - Handle Animation Hold State Here
+                print("Handle Hold")
+            }
+        }
+    }
     //Workout stuff
     
     func startTimer(){
@@ -67,7 +138,7 @@ class BreathingAnimationController: WKInterfaceController {
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (Timer) in
             self.heartRate = self.healthKitManager.heartRate
             
-            var heartRateString = String(format: "%.0f", self.heartRate)
+            let heartRateString = String(format: "%.0f", self.heartRate)
             
             self.heartRateLabel.setText("\(heartRateString) BPM")
             
@@ -105,7 +176,7 @@ class BreathingAnimationController: WKInterfaceController {
             
         }
         
-        //Logicnya (60 / BPM) untuk tahu kecepatan 1 beat itu berapa detik kan lalu dibagi 2 utk membesar dan mengecil nya dibagi 2
+        //Logicnya (60 / BPM) untuk tahu kecepatan 1 beat itu berapa detik kan lalu dibagi 2 utk membesar dan mengecil nya dibagi 2 -- Comment From Javier
           
         
         
