1edae149eca62bef0c23328e53ca0ee087f27caf
Javier Fransiscus <pier9402@gmail.com>
2021-08-24 10:12:55 +0700

    Remove Picker View
    I am going to fix onboarding first then ill be back here

--- PanicAway/Module/Localization/LocalizationMenuViewController.swift
+++ PanicAway/Module/Localization/LocalizationMenuViewController.swift
@@ -9,43 +9,12 @@ import Foundation
 import UIKit
 
 private let userLanguageKey = "applicationKey"
-class LocalizationMenuViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        setLanguange(language: pickerData[row])
-        print(pickerData[row])
-    }
-    
-    @IBAction func confirmSelectedLanguange(_ sender: Any) {
-        navigateToProductShowcase()
-    }
-    
-    func navigateToProductShowcase() {
-        let vc = ProductShowcaseViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @IBOutlet weak var pickerView: UIPickerView!
+class LocalizationMenuViewController: UIViewController{
     
     var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        pickerView.dataSource = self
-        pickerView.delegate = self
         
         // Do any additional setup after loading the view.
         pickerData = ["en","id"]
@@ -56,15 +25,15 @@ class LocalizationMenuViewController: UIViewController, UIPickerViewDelegate, UI
     }
     
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    @IBAction func confirmSelectedLanguange(_ sender: Any) {
+        navigateToProductShowcase()
+    }
+    
+    func navigateToProductShowcase() {
+        let vc = ProductShowcaseViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
     func setLanguange(language: String) {
         //Ini jangan lupa di ganti
         UserDefaults.standard.set(language, forKey: userLanguageKey)
